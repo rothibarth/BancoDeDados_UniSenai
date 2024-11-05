e8903f2e6e19895ad6189b4da15e112bf3951250
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-11-04 21:09:24 -0300

    new

--- OT 03 BANCO DE DADOS/script_mercado_OT03.sql
+++ OT 03 BANCO DE DADOS/script_mercado_OT03.sql
@@ -27,12 +27,19 @@ primary key(idvenda, idproduto));
 SELECT * FROM mercado.vendas_has_produto;
 use mercado;
 
-insert into 'vendas_has_produto' values (1, 1, 5);
-insert into 'vendas_has_produto' values (1, 1, 5);
-insert into 'vendas_has_produto' values (1, 1, 5);
-insert into 'vendas_has_produto' values (1, 1, 5);
-insert into 'vendas_has_produto' values (1, 1, 5);
-insert into 'vendas_has_produto' values (1, 1, 5);
+insert into vendas_has_produto values (1, 1, 5);
+insert into vendas_has_produto values (1, 2, 2);
+insert into vendas_has_produto values (1, 4, 6);
+insert into vendas_has_produto values (2, 1, 2);
+insert into vendas_has_produto values (3, 1, 4);
+insert into vendas_has_produto values (3, 4, 1);
+insert into vendas_has_produto values (4, 1, 3);
+insert into vendas_has_produto values (4, 2, 2);
+insert into vendas_has_produto values (5, 4, 4);
+insert into vendas_has_produto values (6, 2, 1);
+
+
+
 
 
 
