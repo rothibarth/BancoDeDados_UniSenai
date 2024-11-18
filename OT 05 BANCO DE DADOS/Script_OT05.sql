59cc96ad4452c9e79568227b0250128b4e433ee3
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-11-18 20:58:59 -0300

    OT FINALIZADA COM SUCESSO

--- OT 05 BANCO DE DADOS/Script_OT05.sql
+++ OT 05 BANCO DE DADOS/Script_OT05.sql
@@ -12,3 +12,30 @@ rua varchar(45),
 bairro varchar(45),
 numero int(50),
 cidade varchar(45));
+
+/* atvd 2*/
+
+CREATE VIEW vendasDia AS
+SELECT 
+    V.data_venda AS DataVenda,
+    P.descricao AS Produto
+FROM 
+    Vendas V
+JOIN 
+    vendas_has_produto VP ON V.idvenda = VP.idvenda
+JOIN 
+    Produto P ON VP.idproduto = P.idproduto
+ORDER BY 
+    V.data_venda DESC;
+    
+SELECT * FROM vendasDia;
+
+/* atvd 3*/
+
+CREATE VIEW QtdVendas AS
+SELECT 
+    COUNT(*) AS TotalVendas
+FROM 
+    Vendas;
+
+select * from QtdVendas;
